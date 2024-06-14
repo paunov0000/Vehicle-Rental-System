76d936319f2d23bf9cfaba55521b5455bc9ff782
paunov0000 <legendarygriefer96@gmail.com>
2024-06-14 12:53:02 +0300

    Created an IVehicle interface to be implemented by the Car/Motorcycle/CargoVan classes

---
+++ Vehicle-Rental-System/IVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle_Rental_System
+{
+    internal interface IVehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal Value { get; set; }
+        public string CalculateRentalPrice(int rentalPeriod);
+    }
+}
