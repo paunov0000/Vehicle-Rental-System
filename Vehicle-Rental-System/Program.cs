28ac998370c30a510871468f346f3ed5b0601ce7
paunov0000 <legendarygriefer96@gmail.com>
2024-06-14 12:44:32 +0300

    Created a blueprint of how I imagine the user input to be handled

--- Vehicle-Rental-System/Program.cs
+++ Vehicle-Rental-System/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string vehicleBrand = Console.ReadLine()!;
+string vehicleModel = Console.ReadLine()!;
+decimal vehicleValue = decimal.Parse(Console.ReadLine()!);
+string vehicleType = Console.ReadLine()!;
+Vehicle vehicle;
+switch (vehicleType)
+{
+    case "car":
+        int safetyRating = int.Parse(Console.ReadLine()!);
+        vehicle = new Car(vehicleBrand, vehicleModel, vehicleValue, safetyRating);
+        break;
+    case "motorcycle":
+        int riderAge = int.Parse(Console.ReadLine()!);
+        vehicle = new Motorcycle(vehicleBrand, vehicleModel, vehicleValue, riderAge);
+        break;
+    case "cargo van":
+        int driverExperience = int.Parse(Console.ReadLine()!);
+        vehicle = new CargoVan(vehicleBrand, vehicleModel, vehicleValue, driverExperience);
+        break;
+    default:
+        break;
+}
+
+int rentalPeriod = int.Parse(Console.ReadLine()!);
+Console.WriteLine(vehicle.CalculateRentalPrice(rentalPeriod));
